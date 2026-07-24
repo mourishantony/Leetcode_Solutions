44772ba6d1a61ba38394e6e81d7c255860a5754a
MourishAntony <mourishantonyc@gmail.com>
2026-07-24 09:47:37 +0530

    Time: 731 ms (26.83%) | Memory: 28.2 MB (35.69%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,72 +1,39 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def bubble_sort(nums):
-            flag = True
-
-            while flag:
-                flag = False
-                for i in range(1,len(nums)):
-                    if nums[i-1] > nums[i]:
-                        flag = True
-                        nums[i-1],nums[i] = nums[i] , nums[i-1]
-
-        def insertion_sort(nums):
-            n = len(nums)
-            for i in range(1,n):
-                for j in range(i,0,-1):
-                    if nums[j-1] > nums[j]:
-                        nums[j-1] ,nums[j] = nums[j] , nums[j-1]
-                    else:
-                        break
-
-        def selection_sort(nums):
-            n = len(nums)
-            for i in range(n):
-                for j in range(i+1,n):
-                    if nums[i] > nums[j]:
-                        nums[i] , nums[j] = nums[j] , nums[i]
-
         def merge_sort(nums):
-            n = len(nums)
-            if n <2:
+            n= len(nums)
+            if n < 2:
                 return nums
             
             m = n//2
-            L = nums[:m]
-            R = nums[m:]
+            L,R = nums[:m] , nums[m:]
 
             L = merge_sort(L)
             R = merge_sort(R)
 
-            l,r = 0,0
-            L_len = len(L)
-            R_len = len(R)
+            l,r,i = 0,0,0
+            sorted_nums = [0]*n
 
-            i = 0
-            sorted_arr = [0] *n
-
-            while l < L_len and r < R_len:
-                if L[l] <= R[r]:
-                    sorted_arr[i] = L[l]
+            while l < len(L) and r < len(R):
+                if L[l] < R[r]:
+                    sorted_nums[i] = L[l]
                     l+=1
                 else:
-                    sorted_arr[i] = R[r]
+                    sorted_nums[i] = R[r]
                     r+=1
                 i+=1
-            
-            while l < L_len:
-                sorted_arr[i] = L[l]
+
+            while l < len(L):
+                sorted_nums[i] = L[l]
                 l+=1
                 i+=1
-            while r < R_len:
-                sorted_arr[i] = R[r]
+            
+            while r < len(R):
+                sorted_nums[i] = R[r]
                 r+=1
                 i+=1
 
-            return sorted_arr
+            return sorted_nums
 
-        # bubble_sort(nums)
-        # insertion_sort(nums)
-        # selection_sort(nums)
-    
-        return merge_sort(nums)
+        ans = merge_sort(nums)
+        return ans
