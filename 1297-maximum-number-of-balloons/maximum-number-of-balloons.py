d6c0623e79af3851b0f48d8df745cc3d1ad64d12
MourishAntony <mourishantonyc@gmail.com>
2026-07-24 15:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.65%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,21 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        s = "balloon"
-        counted = Counter(text)
-        flag = "True"
-        ans = {}
-        for i in range(len(s)):
-            if s[i] in counted:
-                ans[s[i]] = counted[s[i]]
-            else:
-                return 0
-        count = 0
-        while flag:
-            for i in s:
-                if ans[i] > 0:
-                    ans[i] -=1
-                else:
-                    return count
-            count+=1
-        
-        return count
+        count = Counter(text)
+
+        return min(
+            count['b'],
+            count['a'],
+            count['l']//2,
+            count['o']//2,
+            count['n']
+        )
