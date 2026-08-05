ea8d8443e50f79c891d123f099c86a88b4c64ace
MourishAntony <mourishantonyc@gmail.com>
2026-08-05 11:45:07 +0530

    Time: 23 ms (54.33%) | Memory: 19.5 MB (71.88%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,3 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(ransomNote) <= Counter(magazine)
+        ans = {}
+
+        for a in magazine:
+            ans[a] = ans.get(a,0) + 1
+
+        for a in ransomNote:
+            if ans.get(a,0) == 0:
+                return False
+            ans[a]-=1
+        return True
