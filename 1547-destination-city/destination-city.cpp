2ae89ce6424e545557cfcaa4a3345e52dacd3d5b
MourishAntony <mourishantonyc@gmail.com>
2026-08-06 02:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (63.69%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        set<string> s ={};
+        unordered_set<string> s ={};
         for(auto &path:paths) s.insert(path[0]);
 
         for(auto &path:paths) if(!s.contains(path[1])) return path[1];
