51b3e77a10f821d40c203c403338c54c519620fc
MourishAntony <mourishantonyc@gmail.com>
2026-08-06 16:11:22 +0530

    Time: 16 ms (85.08%) | Memory: 24.1 MB (71.80%) - LeetSync

---
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        double max1 = -INFINITY , max2 = -INFINITY , min1 = INFINITY,min2 = INFINITY;
+        for(int num:nums){
+            if(num >= max1){
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num > max2){
+                max2 = num;
+            }
+
+            if(num <= min1){
+                min2 = min1;
+                min1 = num;
+            }
+            else if(num < min2){
+                min2 = num;
+            }
+        }
+        return max1*max2 - min1*min2;
+    }
+};
