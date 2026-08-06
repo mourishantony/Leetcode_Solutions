e23d99ee2fc4f2d7748d1acbbc2269eaca6e1c41
MourishAntony <mourishantonyc@gmail.com>
2026-08-06 15:45:20 +0530

    Time: 135 ms (84.60%) | Memory: 20.7 MB (15.09%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -4,12 +4,12 @@ class Solution:
         min1,min2 = float('inf') , float('inf')
 
         for num in nums:
-            if num >max1:
+            if num >= max1:
                 max2 = max1
                 max1 = num
             elif num > max2:
                 max2 = num
-            if num<min1:
+            if num<=min1:
                 min2 = min1
                 min1 = num
             elif num < min2:
