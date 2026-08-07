c1a03ce0c7b15655cc01ef3ba8634621b4b9b232
MourishAntony <mourishantonyc@gmail.com>
2026-08-07 10:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.71%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -5,6 +5,9 @@ class Solution:
             sol=i
             while sol>0:
                 temp = sol%10
+                if temp ==0:
+                    add =0
+                    break
                 add*=temp
                 sol= sol//10
             if add %t==0:
