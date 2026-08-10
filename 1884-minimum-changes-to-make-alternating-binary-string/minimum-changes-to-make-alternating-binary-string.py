90502a14ee9492985604a5e696c80d8d0fd246ae
MourishAntony <mourishantonyc@gmail.com>
2026-08-10 16:06:07 +0530

    Time: 9 ms (45.33%) | Memory: 19.2 MB (62.41%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,12 +1,7 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        compare1 = "01"*(int(len(s)//2)+1)
-        compare2 = "10"*(int(len(s)//2)+1)
-        count1 = 0
-        count2 = 0
+        count = 0
         for i in range(len(s)):
-            if compare1[i] != s[i]:
-                count1+=1
-            if compare2[i] != s[i]:
-                count2+=1
-        return min(count1,count2)  
+            if s[i] != str(i%2):
+                count+=1
+        return min(count,len(s) - count)  
