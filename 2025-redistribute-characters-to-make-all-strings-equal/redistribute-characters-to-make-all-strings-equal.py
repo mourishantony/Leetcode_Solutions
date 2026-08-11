638e75aa8e80a1e581f8f5ce1a90b8173cb3ff4e
MourishAntony <mourishantonyc@gmail.com>
2026-08-11 07:29:37 +0530

    Time: 15 ms (54.35%) | Memory: 19.3 MB (88.59%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -1,11 +1,8 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        store = {}
+        store = [0] *26
         for word in words:
             for ch in word:
-                store[ch]  = store.get(ch,0)+1
+                store[ord(ch) -97]  +=1
         
-        for i in store.keys():
-            if store[i] % len(words) !=0:
-                return False
-        return True
+        return all(i % len(words) == 0 for i in store)
