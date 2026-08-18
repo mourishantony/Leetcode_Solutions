8a0690e521acc78b139e2e05f5fc2d921ff9309b
MourishAntony <mourishantonyc@gmail.com>
2026-08-18 11:46:32 +0530

    Time: 24 ms (5.01%) | Memory: 19.5 MB (16.29%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        a = words[0]
+        ans = list(a)
+        for i in a:
+            for j in range(1,len(words)):
+                if i not in set(words[j]):
+                    ans.remove(i)
+                    break
+                else:
+                    words[j] = words[j].replace(i,"",1)
+        return ans
