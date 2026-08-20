aa5a03ad414937e85827f04e1b8eab6fe144c9bb
MourishAntony <mourishantonyc@gmail.com>
2026-08-20 15:42:44 +0530

    Time: 23 ms (32.21%) | Memory: 51.6 MB (50.72%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -1,8 +1,10 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        pos = customers[0][0] + customers[0][1]
-        sec = pos - customers[0][0]
-        for i in range(1,len(customers)):
+        # pos = customers[0][0] + customers[0][1]
+        # sec = pos - customers[0][0]
+        pos = 0
+        sec =0
+        for i in range(len(customers)):
             if customers[i][0] < pos:
                 pos += customers[i][1]
             else:
