b7e906b39423dd26b1bfc5b4bfd3a1aa0b5010cc
MourishAntony <mourishantonyc@gmail.com>
2026-08-20 22:06:53 +0530

    Time: 12 ms (51.14%) | Memory: 31.3 MB (18.36%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         unordered_map<int,string> store;
         for(int i=0;i<names.size();i++) store[heights[i]] = names[i];
-        sort(heights.begin(),heights.end(),greater<int>());
+        sort(heights.rbegin(),heights.rend());
         vector<string> ans;
         for(int i=0;i<names.size();i++) ans.push_back(store[heights[i]]);
         return ans;
