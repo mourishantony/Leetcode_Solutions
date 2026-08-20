c6ab27354a753fd5722f672a8009c913613c5bf5
MourishAntony <mourishantonyc@gmail.com>
2026-08-20 21:48:17 +0530

    Time: 4 ms (60.58%) | Memory: 19.6 MB (52.76%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        ans = []
+        store ={}
+        for i in range(len(names)):
+            store[heights[i]] = names[i]
+        heights.sort(reverse = True)
+        print(heights)
+
+        for i in range(len(names)):
+            ans.append(store[heights[i]])
+        return ans
